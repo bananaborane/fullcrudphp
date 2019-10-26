6fb54d1ddf402272e68264307f8254946959cb03
unknown <fdsa4321jkl@yahoo.com>
2019-10-26 11:38:03 -0700

    first commit to gh

---
+++ php/operation.php
@@ -0,0 +1,135 @@
+<?php
+
+require_once ("db.php");
+require_once ("component.php");
+
+$con = Createdb();
+
+// create button click
+if(isset($_POST['create'])){
+    createData();
+}
+if(isset($_POST['update'])){
+    UpdateData();
+}
+if(isset($_POST['delete'])){
+    deleteRecord();
+}
+if(isset($_POST['deleteall'])){
+    deleteAll();
+}
+
+function createData(){
+    $bookname = textboxValue("book_name");
+    $bookpublisher = textboxValue("book_publisher");
+    $bookprice = textboxValue("book_price");
+    if($bookname && $bookpublisher && $bookprice){
+        $sql = "INSERT INTO books (book_name, book_publisher, book_price) 
+                        VALUES ('$bookname','$bookpublisher','$bookprice')";
+        if(mysqli_query($GLOBALS['con'], $sql)){
+            TextNode("success", "Record Successfully Inserted!");
+        }else{
+            echo "Error";
+        }
+    }else{
+            TextNode("error", "Please Provide Data in the Textbox");
+    }
+}
+
+function textboxValue($value){
+    $textbox = mysqli_real_escape_string($GLOBALS['con'], trim($_POST[$value]));
+    if(empty($textbox)){
+        return false;
+    }else{
+        return $textbox;
+    }
+}
+
+// messages
+function TextNode($classname, $msg){
+    $element = "<h6 class='$classname'>$msg</h6>";
+    echo $element;
+}
+
+// get data from mysql database
+function getData(){
+    $sql = "SELECT * FROM books";
+    $result = mysqli_query($GLOBALS['con'], $sql);
+    if(mysqli_num_rows($result) > 0){
+        return $result;
+    }
+}
+
+// update data
+function UpdateData(){
+    $bookid = textboxValue("book_id");
+    $bookname = textboxValue("book_name");
+    $bookpublisher = textboxValue("book_publisher");
+    $bookprice = textboxValue("book_price");
+    if($bookname && $bookpublisher && $bookprice){
+        $sql = "
+                    UPDATE books SET book_name='$bookname', book_publisher = '$bookpublisher', book_price = '$bookprice' WHERE id='$bookid';                    
+        ";
+        if(mysqli_query($GLOBALS['con'], $sql)){
+            TextNode("success", "Data Successfully Updated!");
+        }else{
+            TextNode("error", "Enable to Update Data");
+        }
+    }else{
+        TextNode("error", "Please Select a Book to Edit");
+    }
+}
+
+// deletes a record
+function deleteRecord(){
+    $bookid = (int)textboxValue("book_id");
+    if($bookid > -1){
+        $sql = "DELETE FROM books WHERE id=$bookid";
+        if(mysqli_query($GLOBALS['con'], $sql)){
+            TextNode("success","Record Deleted Successfully");
+        }else{
+            TextNode("error","Enable to Delete Record");
+        }
+    } else {
+        TextNode("error", "Please Select a Book to Delete");
+    }
+}
+
+// renders delete all button
+function renderDeleteAllBtn(){
+    $result = getData();
+    $i = 0;
+    if($result){
+        while ($row = mysqli_fetch_assoc($result)){
+            $i++;
+            if($i > 2){
+                buttonElement("btn-deleteall", "btn btn-danger" ,"<i class='fas fa-trash'></i> Delete All", "deleteall", "");
+                return;
+            }
+        }
+    }
+}
+
+
+function deleteAll(){
+    $sql = "DROP TABLE IF EXISTS books";
+    if(mysqli_query($GLOBALS['con'], $sql)){
+        TextNode("success","All Record deleted Successfully");
+        Createdb();
+    }else{
+        TextNode("error","Something Went Wrong, Record cannot deleted");
+    }
+}
+
+
+// set id to textbox
+function setID(){
+    $getid = getData();
+    $id = 0;
+    if($getid){
+        while ($row = mysqli_fetch_assoc($getid)){
+            $id = $row['id'];
+        }
+    }
+    return null ||($id + 1);
+}
